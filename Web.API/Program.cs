cdf3f137b7231e6a8d70ef70c6f5ae0f5d11fb40
Daniel Hildebrandt <d.hildebrandt93@gmail.com>
2022-05-06 14:13:34 -0600

    SignalRChat

--- Web.API/Program.cs
+++ Web.API/Program.cs
@@ -1,3 +1,5 @@
+using SignalRChat.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,18 +9,28 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddRazorPages();
+builder.Services.AddSignalR();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI();
+  app.UseExceptionHandler("/Error");
+  app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
 
 app.UseAuthorization();
+
+app.MapRazorPages();
+app.MapHub<ChatHub>("/chatHub");
 
 app.MapControllers();
 
